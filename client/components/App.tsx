91fefec36f8d4fc054e1cfed3a7da2915dcf9a7f
Zachary Hayward <zmhhayward@gmail.com>
2024-06-06 13:14:34 -0500

    Added commented out stuff in App to check useBooking hook

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -6,18 +6,12 @@ import { createContext, useState } from 'react'
 const BookingTimeContext = createContext({ courtNumber: 1, timeslot: '9:30' })
 
 function App() {
-  const bookings = useBookings()
-
-  const { data: dailyBookings } = bookings.byDay(1721044800000)
-
-  // THE FUNCTIONS BELOW WORK BUT INFINITELY, RECURSIVELY LOOP (SEE DATABASE FOR EVIDENCE)
-  // bookings.del(String(14))
-  // bookings.add({ court_id: 3, user_id: 6, date: new Date(1721044800000), start_time: 1100, end_time: 1200, gear_rental: true, price: 20, paid: true })
-
-  const handleAddBooking = () => {
-    bookings.add({ court_id: 3, user_id: 6, date: new Date(1721044800000).getTime(), start_time: 1100, end_time: 1200, gear_rental: true, price: 20, paid: true })
-  }
-  console.log(dailyBookings)
+  // const bookings = useBookings()
+  // const { data: dailyBookings } = bookings.byDay(1721044800000)
+  // const handleAddBooking = () => {
+  //   bookings.add({ court_id: 3, user_id: 6, date: new Date(1721044800000).getTime(), start_time: 1100, end_time: 1200, gear_rental: true, price: 20, paid: true })
+  // }
+  // console.log(dailyBookings)
 
   const [bookingTime, setBookingTime] = useState({
     courtNumber: 1,
@@ -25,7 +19,7 @@ function App() {
   })
   return (
     <>
-      <button onClick={handleAddBooking}>Add a booking</button>
+      {/* <button onClick={handleAddBooking}>Add a booking</button> */}
       <div className="app">
         <h1 className="text-3xl font-bold underline">
           Fullstack Boilerplate - with Fruits!
